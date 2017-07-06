e1fc2b87f1210f56d16d91fa9f7d6e87e86a7bc4
afeiship <1290657123@qq.com>
2017-07-06 11:09:17 +0800

    issue: soft/go -> ~/go

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 # mkdir $HOME/go
-export GOPATH=$HOME/soft/go
+export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:/usr/local/opt/go/libexec/bin
 
