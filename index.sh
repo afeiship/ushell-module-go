09a4f64bbda55fbc190121580e46db48025fd16b
afeiship <1290657123@qq.com>
2017-07-06 10:59:24 +0800

    issue: initial

---
+++ index.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+# mkdir $HOME/go
+export GOPATH=$HOME/soft/go
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
+
+unset ROOT_PATH;
