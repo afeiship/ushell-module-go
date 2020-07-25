0139cb420ae5c766f8573b80052899c320d47276
afeiship <1290657123@qq.com>
2020-07-25 11:16:57 +0800

    chore: use new scripts | npm

--- index.sh
+++ index.sh
@@ -2,9 +2,10 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-# mkdir $HOME/go
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:/usr/local/opt/go/libexec/bin
+# alias xxx='cd -';
+# export GOPATH=$HOME/go
+# export PATH=$PATH:$GOPATH/bin
+# export PATH=$PATH:/usr/local/opt/go/libexec/bin
+alias gor="go run "
 
 unset ROOT_PATH;
