2e40677cfa246dd94d8fba8ca8d8928c23eeb561
afeiship <1290657123@qq.com>
2022-01-21 20:54:25 +0800

    feat: gor alias

--- index.sh
+++ index.sh
@@ -4,7 +4,9 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 export GOPATH=$(go env GOPATH);
 export GOROOT=$(go env GOROOT);
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$GOROOT/bin
+export PATH=$PATH:$GOPATH/bin;
+export PATH=$PATH:$GOROOT/bin;
+
+alias gor="go run";
 
 unset ROOT_PATH;
